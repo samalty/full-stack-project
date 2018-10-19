1d2206ef1a0e28cc47757a9d453034984566affd
Sam Alty <sam_alty@outlook.com>
2018-10-19 10:03:51 +0000

    Removed env from settings

--- project_manager/settings.py
+++ project_manager/settings.py
@@ -130,8 +130,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
-STRIPE_SECRET = os.getenv('STRIPE_SECRET')
+#STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
+#STRIPE_SECRET = os.getenv('STRIPE_SECRET')
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 

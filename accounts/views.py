dd5c3e54f0c5f8594b5f0eec1aa415ea4f7d8757
Sam Alty <sam_alty@outlook.com>
2018-11-21 13:21:29 +0000

    Updated accounts views

--- accounts/views.py
+++ accounts/views.py
@@ -17,9 +17,9 @@ def index(request):
             user = auth.authenticate(username=request.POST['username'],
                                      password=request.POST['password1'])
             UserProfile.objects.create(user=user)
-        if request.user:
+        if user:
             auth.login(user=user,request=request)
-            return redirect(profile, request.user.pk)
+            return redirect(profile, user.pk)
         else:
             messages.error(request, "Sorry. We are unable to register your account at this time.")
     else:
@@ -42,9 +42,9 @@ def login(request):
         if login_form.is_valid():
             user = auth.authenticate(username=request.POST['username'],
                                      password=request.POST['password'])
-            if request.user:
+            if user:
                 auth.login(user=user, request=request)
-                return redirect(profile, request.user.pk)
+                return redirect(profile, user.pk)
             else:
                 login_form.add_error(None, "Sorry. The username and password \
                 combination that you entered wasn't recognised.")
